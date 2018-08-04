f51b3177edcb1cc67f7797ffb6977f2aa3ed4881
phillipchan2 <phillip.chan@liferay.com>
2018-08-04 14:41:26 -0700

    Add Analytics + SF

--- index.php
+++ index.php
@@ -1,210 +1,190 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+	<!-- Basic Page Needs
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta charset="utf-8">
+	<title>Chicago South Side Podiatrist Dr. Phillip Ping Chan</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<!-- Mobile Specific Metas
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- FONT & ICONS
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link href="//fonts.googleapis.com/css?family=Raleway:400,300,600,700,800,900" rel="stylesheet" type="text/css">
+	<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- CSS
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" href="css/skeleton.css">
+	<!-- Favicon
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<link rel="icon" type="image/png" href="images/favicon.png">
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-123392170-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>Chicago South Side Podiatrist Dr. Phillip Ping Chan</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+	function gtag() { dataLayer.push(arguments); }
+	gtag('js', new Date());
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- FONT & ICONS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600,700,800,900" rel="stylesheet" type="text/css">
-  <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
-
+	gtag('config', 'UA-123392170-1');
+	</script>
 </head>
+
 <body>
-
-  <!-- Primary Page Layout
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="hero-section">
-   <div class="container">
-       <div class="row">
-         <div class="six columns">
-           <h1><strong>Dr. Ping Chan, DPM</strong></h1>
-           <h5>Podiatrist, Foot Physician, and Surgeon serving Chicago South Side with excellence and care.</h5>
-       </div>
-   </div>
-</div>
-</div>
-
-
-<div class="message-section section">
-    <div class="container section">
-        <div class="row">
-
-            <div class="twelve columns">
-                <h5><em>"With over 25 years of experience in out patient surgery in hospital and surgical center, Dr. Ping Chan, DPM is a primary choice for podiatrist for Chicago's South Side."</em></h5>
-            </div>
-
-
-
-        </div>
-    </div>
-</div>
-
-
-<div class="feature-section section">
-    <div class="container">
-        <div class="row">
-            <div class="six columns">
-                <h4><i class="fa fa-stethoscope"></i> Services</h4>
-                <p>Primary services provide include, but not limited to:</p>
-                <ul>
-                    <li>Diabetic foot care</li>
-                    <li>Diabetic foot ulcer</li>
-                    <li>Bunion</li>
-                    <li>Corns</li>
-                    <li>Fungal Nail</li>
-                </ul>
-            </div>
-            <div class="six columns">
-                <h4><i class="fa fa-building"></i>  Neighborhoods Served</h4>
-                <p>Dr. Chan works in these Chicago neighborhoods:</p>
-                <ul>
-                    <li>Roseland</li>
-                    <li>Southshore</li>
-                    <li>South Chicago</li>
-                    <li>Englewood</li>
-                    <li>Beverly</li>
-                    <li>South Shore</li>
-                </ul>
-            </div>
-
-
-        </div>
-    </div>
-</div>
-
-<div class="schedule-locations">
-    <div class="container section">
-        <div class="row">
-            <div class="sixteen columns">
-                <h4><i class="fa fa-calendar"></i> Where you can find Dr. Ping Chan</h4>
-
-                <article class="day">
-                    <header>Monday</header>
-                    <div class="address">
-                        11246 Michigan <br/>
-                        Chicago, IL 60628
-                        11:00AM-4:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 821-0820</a>
-                    </div>
-                </article>
-
-                <article class="day">
-                    <header>Tuesday</header>
-                    <div class="address">
-                        8201 S. Ashland <br/>
-                        Chicago, IL 60620 <br/>
-                        11:00AM-1:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 873-3434</a>
-                    </div>
-                </article>
-
-
-                <article class="day">
-                    <header>Wednesday</header>
-
-                    <div class="address">
-                        11246 Michigan <br />
-                        Chicago, IL 606028 <br />
-                        2:00PM-4:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 821-0820</a>
-                    </div>
-                </article>
-
-                <article class="day">
-                    <header>Thursday</header>
-
-                    <div class="address">
-                        9037 Commercial <br/>
-                        Chicago, IL 60617 <br/>
-                        10:00AM-1:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 731-2525</a>
-                    </div>
-
-                    <div class="address">
-                        6032 Halsted <br />
-                        Chicago, IL 60621 <br/>
-                        2:00PM-4:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 651-9393</a>
-                    </div>
-                </article>
-
-                <article class="day">
-                    <header>Saturday</header>
-                    <div class="address">
-                        9037 Commercial <br/>
-                        Chicago, IL 60617 <br/>
-                        10:00AM-3:00PM
-                    </div>
-                    <div class="number">
-                        <a href="tel:821-0820">(773) 731-2525</a>
-                    </div>
-                </article>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<div class="cta-section section">
-    <div class="container">
-        <div class="row">
-            <div class="five columns">
-                <h4><i class="fa fa-money"></i> Payments Accepted</h4>
-                <ul>
-                    <li>Insurance</li>
-                    <li>Public Aid</li>
-                    <li>Medicare</li>
-                </ul>
-            </div>
-            <div class="seven columns">
-                <h4><i class="fa fa-hospital-o"></i> Seeing Dr. Chan</h4>
-                <p class="lead">To see Dr. Chan, come in any of the locations. Walk-in clinic policy. No appointment necessary.</p>
-            </div>
-        </div>
-    </div>
-</div>
-
-<footer>
-    <div class="container">
-        <div class="row">
-            <div class="twelve columns">
-                Copyright 2014 Dr. Ping Chan
-            </div>
-        </div>
-    </div>
-</footer>
-
-
-<!-- End Document
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<!-- Primary Page Layout
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	<div class="hero-section">
+		<div class="container">
+			<div class="row">
+				<div class="six columns">
+					<h1><strong>Dr. Ping Chan, DPM</strong></h1>
+					<h5>Podiatrist, Foot Physician, and Surgeon serving Chicago South Side with excellence and care.</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="message-section section">
+		<div class="container section">
+			<div class="row">
+				<div class="twelve columns">
+					<h5><em>"With over 25 years of experience in out patient surgery in hospital and surgical center, Dr. Ping Chan, DPM is a primary choice for podiatrist for Chicago's South Side."</em></h5>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="feature-section section">
+		<div class="container">
+			<div class="row">
+				<div class="six columns">
+					<h4><i class="fa fa-stethoscope"></i> Services</h4>
+					<p>Primary services provide include, but not limited to:</p>
+					<ul>
+						<li>Diabetic foot care</li>
+						<li>Diabetic foot ulcer</li>
+						<li>Bunion</li>
+						<li>Corns</li>
+						<li>Fungal Nail</li>
+					</ul>
+				</div>
+				<div class="six columns">
+					<h4><i class="fa fa-building"></i>  Neighborhoods Served</h4>
+					<p>Dr. Chan works in these Chicago neighborhoods:</p>
+					<ul>
+						<li>Roseland</li>
+						<li>Southshore</li>
+						<li>South Chicago</li>
+						<li>Englewood</li>
+						<li>Beverly</li>
+						<li>South Shore</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="schedule-locations">
+		<div class="container section">
+			<div class="row">
+				<div class="sixteen columns">
+					<h4><i class="fa fa-calendar"></i> Where you can find Dr. Ping Chan</h4>
+					<article class="day">
+						<header>Monday</header>
+						<div class="address">
+							11246 Michigan
+							<br/> Chicago, IL 60628 11:00AM-4:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 821-0820</a>
+						</div>
+					</article>
+					<article class="day">
+						<header>Tuesday</header>
+						<div class="address">
+							8201 S. Ashland
+							<br/> Chicago, IL 60620
+							<br/> 11:00AM-1:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 873-3434</a>
+						</div>
+					</article>
+					<article class="day">
+						<header>Wednesday</header>
+						<div class="address">
+							11246 Michigan
+							<br /> Chicago, IL 606028
+							<br /> 2:00PM-4:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 821-0820</a>
+						</div>
+					</article>
+					<article class="day">
+						<header>Thursday</header>
+						<div class="address">
+							9037 Commercial
+							<br/> Chicago, IL 60617
+							<br/> 10:00AM-1:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 731-2525</a>
+						</div>
+						<div class="address">
+							6032 Halsted
+							<br /> Chicago, IL 60621
+							<br/> 2:00PM-4:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 651-9393</a>
+						</div>
+					</article>
+					<article class="day">
+						<header>Saturday</header>
+						<div class="address">
+							9037 Commercial
+							<br/> Chicago, IL 60617
+							<br/> 10:00AM-3:00PM
+						</div>
+						<div class="number">
+							<a href="tel:821-0820">(773) 731-2525</a>
+						</div>
+					</article>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="cta-section section">
+		<div class="container">
+			<div class="row">
+				<div class="five columns">
+					<h4><i class="fa fa-money"></i> Payments Accepted</h4>
+					<ul>
+						<li>Insurance</li>
+						<li>Public Aid</li>
+						<li>Medicare</li>
+					</ul>
+				</div>
+				<div class="seven columns">
+					<h4><i class="fa fa-hospital-o"></i> Seeing Dr. Chan</h4>
+					<p class="lead">To see Dr. Chan, come in any of the locations. Walk-in clinic policy. No appointment necessary.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="twelve columns">
+					Copyright 2014 Dr. Ping Chan
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- End Document
+	–––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
+
 </html>
